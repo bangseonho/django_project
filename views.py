6018dc6ab1c47be410ccca9a6e756452af418adc
bangseonho <37283474+bangseonho@users.noreply.github.com>
2018-11-06 00:46:41 +0900

    hide my client_id

---
+++ views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+import urllib.request
+from django.http import HttpResponse
+import json
+
+# Create your views here.
+
+def index(request):
+	if request.method == 'GET':
+		client_id = ""
+		client_secret = ""
+		#search_lang = input("검색할 단어를 입력하세요. : ")
+		encText = urllib.parse.quote("어벤져스")
+		url = "https://openapi.naver.com/v1/search/movie?query=" + encText # json 결과
+		# url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
+		movie_request = urllib.request.Request(url)
+		movie_request.add_header("X-Naver-Client-Id",client_id)
+		movie_request.add_header("X-Naver-Client-Secret",client_secret)
+		response = urllib.request.urlopen(movie_request)
+		rescode = response.getcode()
+		if(rescode==200):
+		    response_body = response.read()
+		    results = json.loads(response_body.decode('utf-8'))
+		    items = results.get("items")
+		    context = {
+		    	"items" : items
+		    }
+		    return render(request, 'search/search.html', context)
+		else:
+		    print("Error Code:" + rescode)
